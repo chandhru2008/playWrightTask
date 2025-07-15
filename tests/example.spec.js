fa40c153f8a6be4dd7eccdf4b72db807ea1393ed
G.S.Chandhru <gschandhru10@gmail.com>
2025-07-15 23:24:00 +0530

    fead : added example test cases

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,18 +2,83 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://omayo.blogspot.com/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle("omaya (QAFox.com)");
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('navigate to another page', async ({ page }) => {
+  await page.goto('https://omayo.blogspot.com/');
+  await expect(page).toHaveTitle("omaya (QAFox.com)");
+  await page.locator('#blogsmenu').hover();
+  await page.locator("//span[text() = 'SeleniumByArun']").click();
+  await expect(page).toHaveTitle('Selenium-By-Arun');
 });
+
+test('reading row and column in table', async ({ page }) => {
+  await page.goto('https://omayo.blogspot.com/');
+  await expect(page).toHaveTitle("omayo (QAFox.com)");
+  const tableHeadings = await page.locator('//table[@id = "table1"]//tbody//tr');
+  const count = await tableHeadings.count();
+  for (let i = 1; i <= count; i++) {
+    const tableContents = await page.locator(`//table[@id = "table1"]//tbody//tr[${i}]//td`);
+    const count = await tableContents.count();
+    for (let j = 1; j <= count; j++) {
+      const textContent = await page.locator(`//table[@id = "table1"]//tbody//tr[${i}]//td[${j}]`).textContent();
+      console.log("Table data row " + i + " column " + j + " data " + textContent)
+    }
+  }
+})
+
+
+test('login with username and password', async ({ page }) => {
+  await page.goto('https://omayo.blogspot.com/');
+  await expect(page).toHaveTitle("omayo (QAFox.com)");
+  await page.locator('//form[@name = "form1"]//input[@type = "text"]').click();
+  await page.locator('//form[@name = "form1"]//input[@type = "text"]').fill("Chandhru");
+  await page.locator('//form[@name = "form1"]//input[@type = "password"]').click();
+  await page.locator('//form[@name = "form1"]//input[@type = "password"]').fill("Chandhru_2006");
+  await page.locator('//form[@name = "form1"]//button').click();
+});
+
+test("getting text from preloaded textbox", async ({ page }) => {
+  await page.goto('https://omayo.blogspot.com/');
+  await expect(page).toHaveTitle("omayo (QAFox.com)");
+  const textContent = await page.locator('//div[@class= "widget HTML"]//div[@class = "widget-content"]//textarea[@rows = "10" and @cols = "30"]').textContent();
+  console.log("Text content : ", textContent)
+});
+
+
+test("checking  button 2 is enabled and button 1 is disabled", async ({ page }) => {
+  await page.goto('https://omayo.blogspot.com/');
+  await expect(page).toHaveTitle("omayo (QAFox.com)");
+  const isDisabledButton1 = await page.locator('//button[@id = "but1"]').isDisabled();
+  const isDisabledButton2 = await page.locator('//button[@id = "but2"]').isDisabled();
+  if (isDisabledButton1 && !isDisabledButton2) {
+    console.log("Button 2 is enabled and Button 1 is disabled");
+  }
+});
+
+
+
+test("checking URL that we have entered in search box", async ({ page }) => {
+  await page.goto('https://omayo.blogspot.com/');
+  await expect(page).toHaveTitle("omayo (QAFox.com)");
+  const query = "Chandhru";
+  await page.locator('input[name="q"]').fill(query);
+  await page.keyboard.press('Enter');
+  await expect(page).toHaveURL(new RegExp(query));
+});
+
+test("Print entire option in dropdown", async ({ page }) => {
+  await page.goto('https://omayo.blogspot.com/');
+  await expect(page).toHaveTitle("omayo (QAFox.com)");
+  const allOptions = await page.locator('//select[@id = "drop1"]//option').count();
+  for(let i = 1; i <= allOptions; i++){
+    const content = await page.locator(`//select[@id = "drop1"]//option[${i}]`)
+    console.log('Option ' + 1 + content);
+  }
+  await page.locator('//select[@id = "drop1"]').click();
+  await page.locator('//select[@id = "drop1"]//option[1]]')
+})
